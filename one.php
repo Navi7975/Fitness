33fae84429846e58bd0e9629f625ff5e9f65f13a
Navi7975 <143005396+Navi7975@users.noreply.github.com>
2024-10-14 16:55:49 +0530

    Update one.php

--- one.php
+++ one.php
@@ -2,8 +2,7 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$stripe_secret_key = "sk_test_51PBepaSHpUEHAc6pBcYEex4cahkrlJ4x5c3KONZxIF6OwV1S4lHTYkViO00QU5d7W2kAuSM7ATjJbgsg7ALn5eAF009XP0LdqK";
-
+$stripe_secret_key = " Add stripe secret code";
 \Stripe\Stripe::setApiKey($stripe_secret_key);
 
 $checkout_session = \Stripe\Checkout\Session::create([
